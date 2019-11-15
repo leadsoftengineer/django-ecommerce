3883a1109612bd6aba29422c80c0d07c15802e9a
Engineering Manager <newsoftmalls@gmail.com>
2019-11-15 15:36:39 +0200

    fifth commit

--- env/djecommerce/pages/views.py
+++ env/djecommerce/pages/views.py
@@ -7,5 +7,12 @@ def home_view(request,*args,**kwargs):
     return render(request,"home.html",{})
 def contact_view(*args,**kwargs):
     return HttpResponse("<h1>Contact</h1>")
-def about_view(*args,**kwargs):
-    return HttpResponse("<h1>About</h1>")
+def about_view(request,*args,**kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list":[1,2,3,4,5,6,7,8,9],
+        "title":"django e-commerce"
+    }
+    return  render(request, "about.html", my_context)
