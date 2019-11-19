5f43cdd6f0a99d3a582e6750d235ae8bdcad1133
Engineering Manager <newsoftmalls@gmail.com>
2019-11-19 17:31:11 +0200

    eighth commit

--- env/djecommerce/products/views.py
+++ env/djecommerce/products/views.py
@@ -1,16 +1,25 @@
+#KYIV MEDIA 19.11.19
 from django.shortcuts import render
 from .forms import ProductForm
 from .models import Product
 # Create your views here.
 def product_create_view(request):
-     form = ProductForm(request.POST or None)
-     if form.is_valid():
-         form.save()
+    #print(request.POST)
+    my_new_title=request.POST.get('title')
+    print(my_new_title)
+     #Product.objects.create(title=my_new_title)
+    context = {}
+    return render(request, "products/product_create.html", context)
 
-     context ={
-               'form': form
-     }
-     return render(request,"products/product_create.html", context)
+# def product_create_view(request):
+#      form = ProductForm(request.POST or None)
+#      if form.is_valid():
+#          form.save()
+#
+#      context ={
+#                'form': form
+#      }
+#      return render(request,"products/product_create.html", context)
 # Create your views here.
 def product_detail_view(request):
      obj = Product.objects.get(id=1)
